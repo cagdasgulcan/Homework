9738c0c0fe19e8bffa38966698ad0e1832f21503
cagdasgulcan <114527609+cagdasgulcan@users.noreply.github.com>
2022-10-05 12:53:08 +0300

    Add files via upload

---
+++ classesWithAttributes/Product.java
@@ -0,0 +1,12 @@
+package classesWithAttributes;
+
+public class Product {
+	
+	//attribute ya da field da denir
+	String name; 
+	int id;
+	String description;
+	double price;
+	int stockAmount;
+	
+}
